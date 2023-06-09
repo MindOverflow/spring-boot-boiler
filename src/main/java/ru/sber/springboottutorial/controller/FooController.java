0560b172b2b387cbfc5a0e3218f299b6beaec462
Коваленко Антон Андреевич <Kovalenko.A.Andr@sberbank.ru>
2023-06-09 16:04:16 +0300

    Точка, где подключен вычислитель собственного правила, по которому определяется получит доступ пользователь или нет для входа в контроллер

--- src/main/java/ru/sber/springboottutorial/controller/FooController.java
+++ src/main/java/ru/sber/springboottutorial/controller/FooController.java
@@ -1,17 +1,16 @@
 package ru.sber.springboottutorial.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.sber.springboottutorial.model.Foo;
 
 @Controller
-public class MainController {
+public class FooController {
 
     // SecurityExpressionRoot.hasPermission(Object targetId, String targetType, Object permission) ->
     // return this.permissionEvaluator.hasPermission(this.authentication, (Serializable) targetId, targetType, permission);
-    //@PreAuthorize("hasPermission(#id, 'Foo', 'read')")
+    @PreAuthorize(value="hasPermission(#id, 'Foo', 'read')")
     @GetMapping("/foos/{id}")
     @ResponseBody
     public Foo findById(@PathVariable long id) {
--- src/main/java/ru/sber/springboottutorial/security/CustomPermissionEvaluator.java
+++ src/main/java/ru/sber/springboottutorial/security/CustomPermissionEvaluator.java
@@ -1,36 +1,36 @@
-//package ru.sber.springboottutorial.security;
-//
-//import org.springframework.security.access.PermissionEvaluator;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.GrantedAuthority;
-//
-//import java.io.Serializable;
-//// @PreAuthorize("hasPermission(#id, 'Foo', 'read')")
-//// @PreAuthorize("hasPermission(#foo, 'write')")
-//public class CustomPermissionEvaluator implements PermissionEvaluator {
-//    @Override
-//    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
-//        if ((authentication == null) || (targetDomainObject == null) || !(permission instanceof String)) {
-//            return false;
-//        }
-//        final String targetType = targetDomainObject.getClass().getSimpleName().toUpperCase();
-//        return hasPrivilege(authentication, targetType, permission.toString().toUpperCase());
-//    }
-//
-//    @Override
-//    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
-//        if ( (authentication == null) || (targetType == null) || !(permission instanceof String) ) {
-//            return false;
-//        }
-//        return hasPrivilege(authentication, targetType.toUpperCase(), permission.toString().toUpperCase());
-//    }
-//
-//    private boolean hasPrivilege(Authentication authentication, String targetType, String permission) {
-//        for (final GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
-//            if (grantedAuthority.getAuthority().startsWith(targetType) && grantedAuthority.getAuthority().contains(permission)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//}
+package ru.sber.springboottutorial.security;
+
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+// @PreAuthorize("hasPermission(#id, 'Foo', 'read')")
+// @PreAuthorize("hasPermission(#foo, 'write')")
+public class CustomPermissionEvaluator implements PermissionEvaluator {
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        if ((authentication == null) || (targetDomainObject == null) || !(permission instanceof String)) {
+            return false;
+        }
+        final String targetType = targetDomainObject.getClass().getSimpleName().toUpperCase();
+        return hasPrivilege(authentication, targetType, permission.toString().toUpperCase());
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        if ( (authentication == null) || (targetType == null) || !(permission instanceof String) ) {
+            return false;
+        }
+        return hasPrivilege(authentication, targetType.toUpperCase(), permission.toString().toUpperCase());
+    }
+
+    private boolean hasPrivilege(Authentication authentication, String targetType, String permission) {
+        for (final GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+            if (grantedAuthority.getAuthority().startsWith(targetType) && grantedAuthority.getAuthority().contains(permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
